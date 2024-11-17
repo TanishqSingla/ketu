51b1ddde0239b11320eb4ccd24e643ae8dcdf0c7
TanishqSingla <tanishqsingla00@gmail.com>
2024-11-17 20:12:33 +0530

    fix: button type

--- src/Button/Button.tsx
+++ src/Button/Button.tsx
@@ -1,6 +1,6 @@
 import { Props, Tag } from "@src/types/types";
 
-export const Button = <T extends Tag>(props: Props<T>) => {
+export const Button = <T extends Tag = 'button'>(props: Props<T>) => {
 	const { as: Component = 'button', children, ...rest } = props;
 
 	return <Component {...rest}>{children}</Component>;
