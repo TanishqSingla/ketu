13bcce806f2a0ed49b9b61fb293ca01a2918e6e8
TanishqSingla <tanishqsingla00@gmail.com>
2025-03-03 16:04:18 +0530

    feat: export tag type

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export { type Props } from "./types/types";
+export { type Props, type Tag } from "./types/types";
 export { Switch, SwitchThumb } from "./Switch/Switch";
 export { Button } from "./Button/Button";
