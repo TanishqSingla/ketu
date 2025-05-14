c2af4d1a494f40c60b741279917221ed5742d771
TanishqSingla <tanishqsingla00@gmail.com>
2025-05-15 04:53:23 +0530

    fix: types

--- src/Button/Button.tsx
+++ src/Button/Button.tsx
@@ -1,7 +1,7 @@
-import { JSXInternal } from "preact/src/jsx";
+import { JSX } from "preact/jsx-runtime";
 import { Props, Tag } from "src/types/types";
 
-export const Button = <T extends Tag = "button">(props: Props<T>): JSXInternal.Element  => {
+export const Button = <T extends Tag = "button">(props: Props<T>): JSX.Element  => {
 	const { as = "button", children, ...rest } = props;
 
 	const Component = as;
